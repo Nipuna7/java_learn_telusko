4083e0337c8048b5b6bd464b29741551a34889c5
Gunasingha KGNN <141006999+Nipuna7@users.noreply.github.com>
2025-01-28 19:34:27 +0000

    learn string

--- Demo.java
+++ Demo.java
@@ -1,5 +1,8 @@
 public class Demo {
     public static void main(String args[]){
-    System.out.println("hello");
+      String name =new String("Nipuna");
+      System.out.println("Hello "+name);
+      System.out.println(name.concat(" Nirodha"));
+      System.out.println(name.charAt(3));
     }
 }
